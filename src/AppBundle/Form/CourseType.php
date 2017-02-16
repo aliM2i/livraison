d9481f6780a3310605e45e331cd40ffaf1b4ca70
Formation <Formation@10.100.200.139>
2017-02-16 13:51:23 +0100

    version 0.9

--- src/AppBundle/Form/CourseType.php
+++ src/AppBundle/Form/CourseType.php
@@ -13,9 +13,28 @@ class CourseType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('pdeRetrait')->add('pdeLivraison')->add('etat')->add('prix')
-                ->add('client')
-                ->add('livreur')        ;
+        $builder->add('pdeRetrait')->add('pdeLivraison')
+                ->add('etat', \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class, array(
+                     'choices'=> array(
+                        'libre'=>'libre',
+                        'prise en charge'=>'prise en charge',
+                        'terminée'=>'terminée',
+                    )
+                ))
+                
+                ->add('prix')
+                
+                ->add('client', \Symfony\Bridge\Doctrine\Form\Type\EntityType::class, array(
+                'class'         => 'AppBundle\Entity\Utilisateur',
+                'query_builder' => function ($repository) { 
+                    return $repository->createQueryBuilder('u')
+                            ->where('u.role = :role')
+                            ->setParameter('role','ROLE_CLIENT'); 
+                },
+                ));
+                
+              
+                
     }
     
     /**
